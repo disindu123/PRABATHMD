d33ff9eb1f3990b7e3e0c56be2ec92cf7fcc454f
disindu123 <disindudamsandu@gmail.com>
2024-12-12 21:50:48 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94775704025"
+const SESSION_ID = "PRABATH-MD~4FAAzYIZ#8ucjGFsV6Fuhk8WXDlRXPKbtvXRE9uVVWOaCh_GpKgk"
 
 
 module.exports = {
